c9803bd342f886a4f650b90f83f327ffbb244c05
Shreya Goyal <sheryagoyal@Sheryas-MacBook-Air.local>
2020-10-04 08:47:13 +0530

    first commit#

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SettingsComponent } from './settings/settings.component';
+
+const routes: Routes = [
+  {
+    path:"setting", component:SettingsComponent
+  }
+  
+  
+  ];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
