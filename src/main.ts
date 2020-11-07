a4d504af2c3fb91d63770bbc7d4b2ee0e92cc62d
Shreya Goyal <sheryagoyal@Sheryas-MacBook-Air.local>
2020-11-07 18:06:33 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,20 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import Amplify ,{Auth} from 'aws-amplify';
 import '@angular/compiler';
-
 import amplify from './aws-exports';
-
 Amplify.configure(amplify);
-
 const oauth = {
-  // Domain name
   domain : 'schoolweb.auth.us-east-1.amazoncognito.com', 
 
-  // Authorized scopes
   scope : ['phone', 'email', 'profile', 'openid','aws.cognito.signin.user.admin'], 
 
-  // Callback URL
-  redirectSignIn : 'http://localhost:4200/dashboard', 
+  redirectSignIn : 'http://localhost:4200/user/', 
 
-  // Sign out URL
   redirectSignOut : 'http://localhost:4200/',
 
-  // 'code' for Authorization code grant, 
-  // 'token' for Implicit grant
   responseType: 'code',
 
-  // optional, for Cognito hosted ui specified options
   options: {
-      // Indicates if the data collection is enabled to support Cognito advanced security features. By default, this flag is set to true.
       AdvancedSecurityDataCollectionFlag : true
   }
 }
